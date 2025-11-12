ade3787a43a61bdedd69ebe3acf0af443d80cb76
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-11-12 11:51:31 +0900

    Update purchase.php

--- 合体php/purchase.php
+++ 合体php/purchase.php
@@ -51,6 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["confirm"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>購入確認 | POCKET ROOM</title>
   <link rel="stylesheet" href="../css-DS/purchase.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
   <div class="container">
