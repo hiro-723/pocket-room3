b9e8f22450f4ba892b95bb2854e7517a5f1d1f81
hiro-723 <2401165@s.asojuku.ac.jp>
2025-11-12 12:16:42 +0900

    Update okyakusamajouhou.php

--- 合体php/okyakusamajouhou.php
+++ 合体php/okyakusamajouhou.php
@@ -32,36 +32,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="../合体css/style.css">
 </head>
 <body>
-    <div class="container">
-        <img src="../kuma/aikon.png" alt="POCKET ROOM ロゴ">
-        <h1>新規会員登録</h1>
-        <form action="rogin.php" method="post">
-            <label>名前</label>
-            <input type="text" name="name" required>
-
-            <label>都道府県</label>
-            <input type="text" name="prefecture" required>
-
-            <label>市区町村</label>
-            <input type="text" name="city" required>
-
-            <label>番地</label>
-            <input type="text" name="address" required>
-
-            <label>建物名・部屋番号</label>
-            <input type="text" name="building">
-
-            <label>電話番号</label>
-            <input type="tel" name="phone" required>
-
-            <label>メールアドレス</label>
-            <input type="email" name="email" required>
-
-            <label>パスワード</label>
-            <input type="password" name="password" required>
-
-            <input type="submit" value="登録する">
-        </form>
-    </div>
+    <div class="register">
+      <img src="../kuma/moji.png" class="moji">
+    <h2>お客様情報</h2>
+    <form>
+      <input type="text" placeholder="名前">
+      <input type="text" placeholder="フリガナ">
+      <input type="text" placeholder="生年月日">
+      <input type="text" placeholder="住所">
+      <input type="text" placeholder="郵便番号">
+      <input type="text" placeholder="電話番号・携帯番号">
+      <input type="email" placeholder="メールアドレス">
+      <input type="password" placeholder="パスワード">
+      <button type="submit">変更する</button>
+    </form>
+  </div>
 </body>
 </html>
