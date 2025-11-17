8673d30a81ecc4369273a1e33f151cf01937b0c9
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-11-17 12:27:25 +0900

    Update favorite.php

--- 合体php/favorite.php
+++ 合体php/favorite.php
@@ -72,7 +72,7 @@ $favorites = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
           <?php foreach ($favorites as $item): ?>
             <div class="item">
-                <img src="<?= htmlspecialchars($item['img']) ?>" class="product-img">
+                <img src="../jpg/<?= htmlspecialchars($item['img']) ?>" class="product-img">
                 <div class="name"><?= htmlspecialchars($item['product_name']) ?></div>
                 <div class="price"><?= htmlspecialchars($item['price']) ?>円</div>
 
