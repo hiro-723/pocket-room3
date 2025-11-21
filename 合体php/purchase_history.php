6576659d0e88b9a498e32858b25bc32c4afb64db
17daa <2401129@s.asojuku.ac.jp>
2025-11-21 10:46:46 +0900

    c

--- 合体php/purchase_history.php
+++ 合体php/purchase_history.php
@@ -61,21 +61,19 @@ $history = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <h1 class="title">POCKET ROOM</h1>
         <h2 class="subtitle">購入された商品</h2>
 
-        <div class="item-card">
-
-            <?php if(empty($history)): ?>
-                <p>購入履歴はありません。</p>
-            <?php else: ?>
-                <?php foreach ($history as $item): ?>
-                    <div class="thumb">
-                        <img src="../jpg/<?=$item['product_id'] ?>.jpg" class="a">
-                        <div class="a"><?= htmlspecialchars($item['product_name']) ?></div>
-                        <div class="a"><?= htmlspecialchars($item['price']) ?>円</div>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
-
+    <div class="items">
+    <?php foreach ($history as $item): ?>
+        <div class="item">
+            <div class="thumb">
+                <img src="../jpg/<?=$item['product_id']?>.jpg">
+            </div>
+            <div class="name"><?= htmlspecialchars($item['product_name']) ?></div>
+            <div class="price"><?= htmlspecialchars($item['price']) ?>円</div>
         </div>
+    <?php endforeach; ?>
+</div>
+
+
     </main>
 
 </div>
