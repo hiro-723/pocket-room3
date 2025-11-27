2c5982c21d147f5d3738599bf66bb39e2f22acc0
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-11-27 12:19:35 +0900

    Update product.php

--- 合体php/product.php
+++ 合体php/product.php
@@ -66,7 +66,8 @@ $is_favorite = $stmt->fetchColumn();
 
         <!-- 商品画像 -->
         <div class="image">
-        <img src="../jpg/<?=$item['product_id'] ?>.jpg" class="a">
+        <img src="../jpg/<?=$product['product_id'] ?>.jpg" class="a">
+
         </div>
 
         <!-- 商品説明 -->
