b4f5b1d421519b0ca5f99a4badfbcb9785ad7b17
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-11-28 12:24:46 +0900

    Update home.php

--- 合体php/home.php
+++ 合体php/home.php
@@ -66,7 +66,7 @@ $username = htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8');
 <form action="search.php" method="get">
 <header>
 <img src="../kuma/moji.png" class="moji" alt="タイトルロゴ">
-<input type="text"  name="keyword" placeholder="検索">
+<input type="text"  name="keyword" placeholder="商品名・色・ジャンルで検索">
 </header>
  
 <section class="carousel">
