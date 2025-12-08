d417fcaf7b0a755d3646d00ced0b4958724ee52e
onakasuitane <2401175@s.asojuku.ac.jp>
2025-12-08 12:26:01 +0900

    ボタン変更

--- 合体php/rogout.php
+++ 合体php/rogout.php
@@ -21,8 +21,10 @@
       <img src="../kuma/moji.png" class="moji">
       <section class="logout-box">
         <h2>ログアウトしますか？</h2>
-        <button onclick="location.href='rogin.php'" class="buttons">はい</button>
-        <button onclick="location.href='home.php'" class="buttons">いいえ</button>
+        <div class="button-group">
+          <button onclick="location.href='rogin.php'" class="buttons">はい</button>
+          <button onclick="location.href='home.php'" class="buttons">いいえ</button>
+        </div>
       </section>
     </main>
   </div>
